8553c334d697d3e881de935f465c6a69dd4d5a5c
Mateusz Żak <mateusz.zak@robotec.ai>
2024-04-04 13:26:23 +0200

    apply review fixes
    Signed-off-by: Mateusz Żak <mateusz.zak@robotec.ai>

--- Gems/RobotecSplineTools/Code/Source/Tools/SplineToolsEditorComponent.h
+++ Gems/RobotecSplineTools/Code/Source/Tools/SplineToolsEditorComponent.h
@@ -44,6 +44,6 @@ namespace SplineTools
         void ReloadCSVAsset();
         AZStd::vector<AZ::Vector3> GetSplinePointsFromCsv(const AZStd::string& csvFilePath);
         void SaveCsvAsset();
-        bool SetSplinePointsToCsv(const AZStd::string& csvFilePath, const AZStd::vector<AZ::Vector3>& vertices);
+        void SavePointsToCsv(const AZStd::string& csvFilePath, const AZStd::vector<AZ::Vector3>& vertices);
     };
 } // namespace SplineTools
