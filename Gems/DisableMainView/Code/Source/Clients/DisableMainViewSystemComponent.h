cae2f7d6ad526b4ae6ca55230e24baf4453b0289
Michał Pełka <michal.pelka@robotec.ai>
2024-07-08 18:37:23 +0200

    Change disable mechanism to create invalid FOV.
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/DisableMainView/Code/Source/Clients/DisableMainViewSystemComponent.h
+++ Gems/DisableMainView/Code/Source/Clients/DisableMainViewSystemComponent.h
@@ -4,38 +4,37 @@
 #include <AzCore/Component/Component.h>
 #include <AzCore/Component/TickBus.h>
 #include <DisableMainView/DisableMainViewBus.h>
-
-namespace DisableMainView {
+namespace DisableMainView
+{
     class DisableMainViewSystemComponent
-            : public AZ::Component {
+        : public AZ::Component
+        , private AZ::SystemTickBus::Handler
+    {
     public:
         AZ_COMPONENT_DECL(DisableMainViewSystemComponent);
 
-        static void Reflect(AZ::ReflectContext *context);
+        static void Reflect(AZ::ReflectContext* context);
 
-        static void GetProvidedServices(AZ::ComponentDescriptor::DependencyArrayType &provided);
+        static void GetProvidedServices(AZ::ComponentDescriptor::DependencyArrayType& provided);
 
-        static void GetIncompatibleServices(AZ::ComponentDescriptor::DependencyArrayType &incompatible);
+        static void GetIncompatibleServices(AZ::ComponentDescriptor::DependencyArrayType& incompatible);
 
-        static void GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType &required);
+        static void GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required);
 
-        static void GetDependentServices(AZ::ComponentDescriptor::DependencyArrayType &dependent);
+        static void GetDependentServices(AZ::ComponentDescriptor::DependencyArrayType& dependent);
 
         DisableMainViewSystemComponent() = default;
 
         ~DisableMainViewSystemComponent() = default;
 
     protected:
-
-        ////////////////////////////////////////////////////////////////////////
         // AZ::Component interface implementation
         void Init() override;
-
         void Activate() override;
-
         void Deactivate() override;
-        ////////////////////////////////////////////////////////////////////////
 
+        // AZ::SystemTickBus::Handler interface implementation
+        void OnSystemTick() override;
     };
 
 } // namespace DisableMainView
