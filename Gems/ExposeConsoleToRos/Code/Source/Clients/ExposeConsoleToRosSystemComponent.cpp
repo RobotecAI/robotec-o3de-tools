3c3bc60f0c1833fd927e9a95194bca166adda032
Michał Pełka <michal.pelka@robotec.ai>
2024-08-21 17:22:21 +0200

    Aded gem functionality
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/ExposeConsoleToRos/Code/Source/Clients/ExposeConsoleToRosSystemComponent.cpp
+++ Gems/ExposeConsoleToRos/Code/Source/Clients/ExposeConsoleToRosSystemComponent.cpp
@@ -29,34 +29,36 @@ namespace ExposeConsoleToRos
 
     void ExposeConsoleToRosSystemComponent::GetRequiredServices([[maybe_unused]] AZ::ComponentDescriptor::DependencyArrayType& required)
     {
-    }
-
-    void ExposeConsoleToRosSystemComponent::GetDependentServices([[maybe_unused]] AZ::ComponentDescriptor::DependencyArrayType& dependent)
-    {
-    }
-
-    ExposeConsoleToRosSystemComponent::ExposeConsoleToRosSystemComponent()
-    {
-    }
-
-    ExposeConsoleToRosSystemComponent::~ExposeConsoleToRosSystemComponent()
-    {
-    }
-
-    void ExposeConsoleToRosSystemComponent::Init()
-    {
+        required.push_back(AZ_CRC_CE("ROS2Service"));
     }
 
     void ExposeConsoleToRosSystemComponent::Activate()
     {
+        auto ros2Node = ROS2::ROS2Interface::Get()->GetNode();
+        AZ_Assert(ros2Node, "ROS2 node is not available");
+        m_consolePublisher = ros2Node->create_publisher<std_msgs::msg::String>("o3de_console_out", 10);
+        m_consoleSubscription = ros2Node->create_subscription<std_msgs::msg::String>("o3de_console_in", 10,
+            [](const std_msgs::msg::String::SharedPtr msg) {
+                AZ_Printf("ExposeConsoleToRos", "Received message: %s", msg->data.c_str());
+                AzFramework::ConsoleRequestBus::Broadcast(&AzFramework::ConsoleRequests::ExecuteConsoleCommand, msg->data.c_str());
+            });
+        AzFramework::ConsoleNotificationBus::Handler::BusConnect();
+    }
+
+    void ExposeConsoleToRosSystemComponent::OnConsoleCommandExecuted(const char* command)
+    {
+        if (m_consolePublisher){
+            std_msgs::msg::String msg;
+            msg.data = command;
+            m_consolePublisher->publish(msg);
+        }
     }
 
     void ExposeConsoleToRosSystemComponent::Deactivate()
     {
-    }
-
-    void ExposeConsoleToRosSystemComponent::OnTick([[maybe_unused]] float deltaTime, [[maybe_unused]] AZ::ScriptTimePoint time)
-    {
+        AzFramework::ConsoleNotificationBus::Handler::BusDisconnect();
+        m_consolePublisher.reset();
+        m_consoleSubscription.reset();
     }
 
 } // namespace ExposeConsoleToRos
--- Gems/ExposeConsoleToRos/Code/Source/Clients/ExposeConsoleToRosSystemComponent.h
+++ Gems/ExposeConsoleToRos/Code/Source/Clients/ExposeConsoleToRosSystemComponent.h
@@ -3,11 +3,15 @@
 
 #include <AzCore/Component/Component.h>
 #include <AzCore/Component/TickBus.h>
+#include "ROS2/ROS2Bus.h"
+#include "std_msgs/msg/string.hpp"
+#include <AzFramework/Components/ConsoleBus.h>
 
 namespace ExposeConsoleToRos
 {
     class ExposeConsoleToRosSystemComponent
         : public AZ::Component
+        , public AzFramework::ConsoleNotificationBus::Handler
     {
     public:
         AZ_COMPONENT_DECL(ExposeConsoleToRosSystemComponent);
@@ -17,21 +21,20 @@ namespace ExposeConsoleToRos
         static void GetProvidedServices(AZ::ComponentDescriptor::DependencyArrayType& provided);
         static void GetIncompatibleServices(AZ::ComponentDescriptor::DependencyArrayType& incompatible);
         static void GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required);
-        static void GetDependentServices(AZ::ComponentDescriptor::DependencyArrayType& dependent);
 
-        ExposeConsoleToRosSystemComponent();
-        ~ExposeConsoleToRosSystemComponent();
+        ExposeConsoleToRosSystemComponent() = default;
+        ~ExposeConsoleToRosSystemComponent() = default;
 
     protected:
+        // AzFramework::ConsoleNotificationBus::Handler
+        void OnConsoleCommandExecuted(const char* command) override;
 
-        ////////////////////////////////////////////////////////////////////////
         // AZ::Component interface implementation
-        void Init() override;
         void Activate() override;
         void Deactivate() override;
-        ////////////////////////////////////////////////////////////////////////
 
-
+        std::shared_ptr<rclcpp::Subscription<std_msgs::msg::String>> m_consoleSubscription;
+        std::shared_ptr<rclcpp::Publisher<std_msgs::msg::String>> m_consolePublisher;
     };
 
 } // namespace ExposeConsoleToRos
