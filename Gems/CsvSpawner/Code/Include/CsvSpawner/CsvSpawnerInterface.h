f70295a8f570511381e00aa8c3d20a65a3b6361a
Wojciech Czerski <wojciech.czerski@robotec.ai>
2025-01-28 16:53:39 +0100

    init | add notification bus interface class
    Signed-off-by: Wojciech Czerski <wojciech.czerski@robotec.ai>

---
+++ Gems/CsvSpawner/Code/Include/CsvSpawner/CsvSpawnerInterface.h
@@ -0,0 +1,45 @@
+/**
+* Copyright (C) Robotec AI - All Rights Reserved
+ *
+ * This source code is protected under international copyright law.  All rights
+ * reserved and protected by the copyright holders.
+ * This file is confidential and only available to authorized individuals with
+ * the permission of the copyright holders.  If you encounter this file and do
+ * not have permission, please contact the copyright holders and delete this
+ * file.
+ */
+
+#pragma once
+
+#include <AzCore/EBus/EBus.h>
+#include <AzCore/std/string/string.h>
+
+namespace CsvSpawner
+{
+    class CsvSpawnerInterface
+        : public AZ::EBusTraits
+    {
+    public:
+        virtual ~CsvSpawnerInterface() = default;
+
+        virtual void OnEntitiesSpawnBegin(
+            const AZStd::vector<CsvSpawnerUtils::CsvSpawnableEntityInfo>& entitiesToSpawn,
+            const AZStd::unordered_map<AZStd::string, CsvSpawnerUtils::CsvSpawnableAssetConfiguration>& spawnableAssetConfiguration,
+            const AZStd::string& physicsSceneName,
+            const AZ::EntityId& parentId) = 0;
+
+        virtual void OnEntitiesSpawnFinished(
+            const AZStd::vector<CsvSpawnerUtils::CsvSpawnableEntityInfo>& entitiesToSpawn,
+            const AZStd::unordered_map<AZStd::string, CsvSpawnerUtils::CsvSpawnableAssetConfiguration>& spawnableAssetConfiguration,
+            const AZStd::string& physicsSceneName,
+            const AZ::EntityId& parentId,
+            const AZStd::unordered_map<int, AzFramework::EntitySpawnTicket>& spawnTickets) = 0;
+
+        // EBus Configuration - allow multiple listeners
+        static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Multiple;
+    };
+
+    // Create an EBus using the notification interface
+    using CsvSpawnerNotificationBus = AZ::EBus<CsvSpawnerInterface>;
+
+} // namespace CsvSpawner
