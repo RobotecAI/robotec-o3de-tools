25178011d21babd6c79b142e64bfb748e24f93b9
Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>
2025-03-17 16:17:42 +0100

    ImGuiProvider Gem for managing displaying of ImGui GUI
    Signed-off-by: Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>

---
+++ Gems/ImGuiProvider/Code/Platform/Windows/imguiprovider_private_files.cmake
@@ -0,0 +1,8 @@
+
+# Platform specific files for Windows
+# i.e. ../Source/Windows/ImGuiProviderWindows.cpp
+#      ../Source/Windows/ImGuiProviderWindows.h
+#      ../Include/Windows/ImGuiProviderWindows.h
+
+set(FILES
+)
