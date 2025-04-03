2fb02a7774f07ab7abb9d7a6132e677c8b587b8c
Wojciech Czerski <wojciech.czerski@robotec.ai>
2025-04-03 14:06:18 +0200

    fix formatting
    Signed-off-by: Wojciech Czerski <wojciech.czerski@robotec.ai>

--- Gems/FPSProfiler/Code/fpsprofiler_private_files.cmake
+++ Gems/FPSProfiler/Code/fpsprofiler_private_files.cmake
@@ -2,8 +2,8 @@
 set(FILES
     Source/FPSProfilerModuleInterface.cpp
     Source/FPSProfilerModuleInterface.h
-        Source/Clients/FPSProfilerComponent.cpp
-        Source/Clients/FPSProfilerComponent.h
+    Source/Clients/FPSProfilerComponent.cpp
+    Source/Clients/FPSProfilerComponent.h
     Source/Configurations/FPSProfilerConfig.cpp
     Source/Configurations/FPSProfilerConfig.h
 )
