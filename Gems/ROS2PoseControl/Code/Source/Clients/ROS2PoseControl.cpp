c3bd4a957bf2a11fba09840dc9c4572d354a85b8
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2026-03-24 10:53:57 +0100

    Migrate to 2605
    Signed-off-by: Piotr Jaroszek <piotr.jaroszek@robotec.ai>

--- Gems/ROS2PoseControl/Code/Source/Clients/ROS2PoseControl.cpp
+++ Gems/ROS2PoseControl/Code/Source/Clients/ROS2PoseControl.cpp
@@ -20,13 +20,11 @@
 #include <AzFramework/Physics/Common/PhysicsSceneQueries.h>
 #include <AzFramework/Physics/PhysicsScene.h>
 #include <AzFramework/Physics/PhysicsSystem.h>
-#include <AzFramework/Physics/SimulatedBodies/RigidBody.h>
 #include <Georeferencing/GeoreferenceBus.h>
 #include <LmbrCentral/Scripting/TagComponentBus.h>
 #include <ROS2/Frame/ROS2FrameComponent.h>
 #include <ROS2/Utilities/ROS2Conversions.h>
-#include <ROS2/Utilities/ROS2Names.h>
-#include <RigidBodyComponent.h>
+#include <Source/RigidBodyComponent.h>
 #include <imgui/imgui.h>
 #include <tf2_ros/transform_listener.h>
 
@@ -124,11 +122,15 @@ namespace ROS2PoseControl
                 AZ_Error("ROS2PoseControl", false, "ROS2PoseControl requires a ROS2FrameComponent to be present on the entity.");
                 return;
             }
-            m_odomFrameId = ros2_frame_component->GetGlobalFrameName();
+            m_odomFrameId = ros2_frame_component->GetGlobalFrameID();
 
             // Initialize the pose subscription
-            auto namespaced_topic_name =
-                ROS2::ROS2Names::GetNamespacedName(ros2_frame_component->GetNamespace(), m_configuration.m_poseTopicConfiguration.m_topic);
+            AZStd::string namespaced_topic_name;
+            ROS2::ROS2NamesRequestBus::BroadcastResult(
+                namespaced_topic_name,
+                &ROS2::ROS2NamesRequestBus::Events::GetNamespacedName,
+                ros2_frame_component->GetNamespace(),
+                m_configuration.m_poseTopicConfiguration.m_topic);
 
             m_poseSubscription = ros2Node->create_subscription<geometry_msgs::msg::PoseStamped>(
                 namespaced_topic_name.data(),
